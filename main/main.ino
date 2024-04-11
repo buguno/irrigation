0df86854aaa7d729cb8c20acc6be32058b240121
buguno <brunnodesouzabezerra@gmail.com>
2024-04-11 19:23:22 -0300

    refactor: moving read humidity function to sensors file

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,3 @@
-int readHumidity(void) {
-  Serial.println("Capacitive Soil Moisture Sensor");
-  int adcValue = analogRead(0);
-  const int adcAirReading = 706;
-  const int adcWaterReading = 284;
-
-  Serial.print("ADC Reading: ");
-  Serial.println(adcValue);
-  int soilMoisturePercentage = map(adcValue, adcAirReading, adcWaterReading, 0, 100);
-
-  Serial.print("Percent Humidity: ");
-  Serial.print(soilMoisturePercentage);
-  Serial.println("%");
-
-  return soilMoisturePercentage;
-}
-
 void setup() {
   Serial.begin(115200);
   delay(2000);
