a803315972f5339c0f256eca900782e11b2c6f22
buguno <brunnodesouzabezerra@gmail.com>
2024-05-10 10:54:40 -0300

    chore: changing timezone calculus

--- main/services.ino
+++ main/services.ino
@@ -2,7 +2,7 @@
 #include <NTPClient.h>
 
 const char* ntpServer = "a.st1.ntp.br";
-const int timeZone = -10800;
+const int timeZone = -3 * 3600;
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, ntpServer, timeZone, 60000);
