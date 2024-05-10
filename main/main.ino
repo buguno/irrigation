bf2a72c30aa7cb85dafee9b64c81c76388055319
buguno <brunnodesouzabezerra@gmail.com>
2024-05-10 13:48:32 -0300

    feat: adding get hour in condition and changing main delays

--- main/main.ino
+++ main/main.ino
@@ -10,17 +10,18 @@ void setup() {
 }
 
 void loop() {
-  delay(30);
   char thingSpeakDataString[100] = {0};
   int humidity = readHumidity();
 
-  if (humidity > 100) {
-    sprintf(thingSpeakDataString, "field1=%d&field2=%d&field3=%d", humidity, 0, 1);
-    sendDataToThingSpeak(thingSpeakDataString);
-    Serial.println("Capacitive Soil Moisture Sensor Error!!!");
-  } else if (humidity < 40) {
-    irrigate(humidity, millis());
+  if (getHour() == 5 || getHour() == 17) {
+    if (humidity > 100) {
+      sprintf(thingSpeakDataString, "field1=%d&field2=%d&field3=%d", humidity, 0, 1);
+      sendDataToThingSpeak(thingSpeakDataString);
+      Serial.println("Capacitive Soil Moisture Sensor Error!!!");
+    } else if (humidity < 40) {
+      irrigate(humidity);
+    }
   }
 
-  delay(60000);
+  delay(1200000);
 }
