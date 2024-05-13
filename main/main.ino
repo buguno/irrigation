b0dcf272cb0f53d3115b94880cf23619ba46404c
buguno <brunnodesouzabezerra@gmail.com>
2024-05-13 14:18:01 -0300

    feat: adding irrigation diff time in loop delay

--- main/main.ino
+++ main/main.ino
@@ -12,19 +12,23 @@ void setup() {
 void loop() {
   char thingSpeakDataString[100] = {0};
   int humidity = readHumidity();
+  long irrigationStarted = 0;
+  long irrigationFinished = 0;
 
   if (getHour() == 5 || getHour() == 17) {
-    if (humidity > 100) {
+    if (humidity > 100 || humidity < -70) {
       sprintf(thingSpeakDataString, "field1=%d&field2=%d&field3=%d", humidity, 0, 1);
       sendDataToThingSpeak(thingSpeakDataString);
       Serial.println("Capacitive Soil Moisture Sensor Error!!!");
     } else if (humidity < 40) {
+      irrigationStarted = millis();
       irrigate(humidity);
+      irrigationFinished = millis();
     }
   } else {
     sprintf(thingSpeakDataString, "field1=%d&field2=%d&field3=%d", humidity, 0, 0);
     sendDataToThingSpeak(thingSpeakDataString);
   }
 
-  delay(1200000);
+  delay(1200000 - (irrigationFinished - irrigationStarted));
 }
