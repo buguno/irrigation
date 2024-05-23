81a7e0e3b897492716c05e55c4abb9157a33c5eb
buguno <brunnodesouzabezerra@gmail.com>
2024-05-23 17:07:01 -0300

    fix(adc-reading): fixing adc values

--- main/sensors.ino
+++ main/sensors.ino
@@ -1,12 +1,12 @@
 int readHumidity(void) {
   Serial.println("Capacitive Soil Moisture Sensor");
   int adcValue = analogRead(0);
-  const int adcAirReading = 706;
-  const int adcWaterReading = 284;
+  const int adcAirReading = 634;
+  const int adcWaterReading = 304;
 
   Serial.print("ADC Reading: ");
   Serial.println(adcValue);
-  int soilMoisturePercentage = map(adcValue, adcAirReading, adcWaterReading, 0, 100);
+  int soilMoisturePercentage = map(adcValue, adcWaterReading, adcAirReading, 100, 0);
 
   Serial.print("Percent Humidity: ");
   Serial.print(soilMoisturePercentage);
